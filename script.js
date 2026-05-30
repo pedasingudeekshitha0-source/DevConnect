284d7eb1c9bbc61ee517e3d0c2bd63b71466467e
Deekshitha Pedasingu <pedasingudeekshitha0@gmail.com>
2026-05-30 16:08:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function searchDevelopers() {
+
+    let input = document.getElementById("searchInput").value.toLowerCase();
+
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        let text = card.innerText.toLowerCase();
+
+        if (text.includes(input)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+}
